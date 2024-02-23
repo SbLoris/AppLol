149832c011c7e79f9cd0c3ba5f109487277a17bc
SbLoris <loris.lebreton@hotmail.com>
2024-02-23 16:21:41 +0100

    commit a

--- lib/champion.dart
+++ lib/champion.dart
@@ -1,13 +1,25 @@
 class Champion {
-  final String id;
-  final String name;
+  final String champname;
+  final String champdescription;
+  final String champicon;
+  final String type;
+  final String role;
 
-  Champion({required this.id, required this.name});
+  Champion({
+    required this.champname,
+    required this.champdescription,
+    required this.champicon,
+    required this.type,
+    required this.role,
+  });
 
   factory Champion.fromJson(Map<String, dynamic> json) {
     return Champion(
-      id: json['id'],
-      name: json['name'],
+      champname: json['champname'],
+      champdescription: json['champdescription'],
+      champicon: json['champicon'],
+      type: json['type'],
+      role: json['role'],
     );
   }
 }
